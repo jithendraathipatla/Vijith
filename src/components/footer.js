ad71afb0a0687b8032a1a54ee10e5807e6532da7
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-06-02 17:28:45 +0530

    making the footer look attractive

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,23 +6,23 @@ const Footer = () => {
     return (
         <div css={fostyle}>
            <div>
-            <TitleComponent title="Vijith Public School"/>  
+            <TitleComponent title="Vijith Public School" color="#ffff"/>  
             <span>ljrehgekjghdkjfg jkfshdfkjsdf sdkhgsggg sskjdfhsdf</span>  
             <span>ljrehgekjghdkjfg jkfshdfkjsdf sdkhgsggg sskjdfhsdf</span>  
             <span>ljrehgekjghdkjfg jkfshdfkjsdf sdkhgsggg sskjdfhsdf</span>         
            </div>
 
            <div>
-           <TitleComponent title="Quick Links"/>
+           <TitleComponent title="Quick Links" color="#ffff"/>
            fglkdfgk           
           </div>
 
           <div>
-          <TitleComponent title="Follows Us"/>           
+          <TitleComponent title="Follows Us" color="#ffff"/>           
          </div>
 
          <div>
-         <TitleComponent title="Contact US"/>           
+         <TitleComponent title="Contact US" color="#ffff"/>           
         </div>
         </div>
     );
@@ -35,8 +35,13 @@ padding:10px 30px;
 display:flex;
 justify-content: space-between;
 box-shadow: 0px 0px 2px 2px rgba(53, 51, 51, 0.68);
+background:#232F3E;
+color:#ffff;
 span{
     display: block;
 }
+font-size: 13px;
+    line-height: 20px;
+    letter-spacing: 0.5px;
 
 `
