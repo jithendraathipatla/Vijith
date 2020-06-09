da5f8b55bf1a380e2f86c077584f44172c8b9970
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-06-09 11:31:50 +0530

    fixing the form details

--- src/components/form.js
+++ src/components/form.js
@@ -1,107 +1,119 @@
-import React from 'react';
-import {css} from '@emotion/core'
+import React from "react"
+import { css } from "@emotion/core"
+import { makeStyles } from "@material-ui/core/styles"
+import TextField from "@material-ui/core/TextField"
+import '../components/layout.css'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}))
 
 const Form = () => {
-    const handelcontactform = () => {
-        console.log("hi")
-    }
-    return (
-        <div css={contact}>
-          <form onSubmit={handelcontactform}>
-         
-          <label>Name:</label>
-          <input name="name" placeholder="Enter Your Name" required/>
-         
+  const classes = useStyles();
+  const [currency, setCurrency] = React.useState('EUR');
 
-         
-          <label>Email:</label>
-          <input name="email" placeholder="Enter Your Email" required/>
-         
-         
-          <label>Phone Number:</label>
-          <input name="phone" type="number" placeholder="Enter Your Phone Number" required/>
-          
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
 
-          <label>Your Locality:</label>
-          <input name="phone" placeholder="Ex: Murakambuttu" required/>
-          
-          <label>Query Regarding:</label>
-          <select name="dropdown">
-           <option>Admission</option>
-           <option>Job Opening</option>
-          </select>
+  const currencies = [
+    {
+      value: "admission",
+      label: "Admission",
+    },
+    {
+      value: "jobopening",
+      label: "Job Opening",
+    },
+  ]
 
-          
+  const handelcontactform = () => {
+    console.log("hi")
+  }
+  return (
+    <div css={contact}>
+      <form onSubmit={handelcontactform} className={classes.root}>
+        <TextField
+          id="outlined-basic"
+          label="Enter Your Name"
+          variant="outlined"
+          required
+        />
 
-          <div style={{textAlign:"center"}}>
-          <button type="submit" css={button}>Submit</button>
-          </div>
-          </form>
+        <TextField
+          id="outlined-basic"
+          label="Enter Your Email"
+          variant="outlined"
+          required
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Enter Your Phone Number"
+          variant="outlined"
+          required
+        />
+
+        <TextField
+          id="outlined-basic"
+          label="Your Locality"
+          variant="outlined"
+          required
+        />
+
+        <div style={{marginTop:"20px"}}>
+        <TextField
+          id="outlined-select-currency-native"
+          select
+          label="Query Regarding"
+          value={currency}
+          onChange={handleChange}
+          SelectProps={{
+            native: true,
+          }}
+          variant="outlined"
+        >
+          {currencies.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </TextField>
         </div>
-    );
-};
+        <div style={{ textAlign: "center", marginTop:"20px" }}>
+          <button type="submit" css={button}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
-export default Form;
+export default Form
 
 const button = css`
- background:#1976d2;
- color:#fff;
- border: none;
- padding: 3px 17px;
- box-shadow:0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12);
- margin-bottom:0px;
+  background: #1976d2;
+  color: #fff;
+  border: none;
+  padding: 3px 17px;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  margin-bottom: 0px;
 `
 
 const contact = css`
-form{
-    margin-bottom:0px;
-}
- label{
-    display: block;
-    font-size: 16px;
-    color: #3c4257;
-    font-weight: 550;
-    margin-top: 10px;
-    letter-spacing: 0.7px;
-    margin-bottom: 10px;
- }
-
- input{
-     margin-bottom:20px;
-     width:100%;
-     display: block;
-     padding: 15px 4px 8px 4px;
-     border: 1px solid #917878a3;
-     width: auto;
-     margin-top: 11px;
-     border-radius: 5px;
-     -webkit-letter-spacing: 1px;
-     -moz-letter-spacing: 1px;
-     -ms-letter-spacing: 1px;
-     letter-spacing: 1px;
-     line-height: 14px;
-     width: -webkit-fill-available;
- }
-
- select{
-    margin-bottom:20px;
-    width:100%;
-    display: block;
-    padding: 15px 4px 8px 4px;
-    border: 1px solid #917878a3;
-    width: auto;
-    margin-top: 11px;
-    border-radius: 5px;
-    -webkit-letter-spacing: 1px;
-    -moz-letter-spacing: 1px;
-    -ms-letter-spacing: 1px;
-    letter-spacing: 1px;
-    line-height: 14px;
-    width: -webkit-fill-available;
-}
-box-shadow: 0px 0px 2px 1px rgba(0,0,0,0.21);
-    background-color: white;
-    margin: 0px 5px;
-    padding:18px;
-
+  form {
+    margin-bottom: 0px;
+  }
+ 
+  box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.21);
+  background-color: white;
+  margin: 0px 5px;
+  padding: 18px;
 `
--- src/components/location.js
+++ src/components/location.js
@@ -11,7 +11,7 @@ const Location = () => {
           <div style={{textAlign:"center"}}>
            <TitleComponent title="location"/>
           </div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15536.764720591842!2d79.08870671907948!3d13.213310720376846!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd4a284ca5b0a3de8!2sVijith%20Public%20School!5e0!3m2!1sen!2sin!4v1591260484136!5m2!1sen!2sin" width="100%" height="550" frameBorder="0"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15536.764720591842!2d79.08870671907948!3d13.213310720376846!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd4a284ca5b0a3de8!2sVijith%20Public%20School!5e0!3m2!1sen!2sin!4v1591260484136!5m2!1sen!2sin" width="100%" height="380" frameBorder="0"></iframe>
           </div>
           <div css={thirs}>
             <div style={{textAlign:"center"}}><TitleComponent title="Contact Us"/></div>
@@ -38,7 +38,7 @@ const first = css`
 `
 
 const second = css`
- flex: 4;
+ flex: 5;
 `
 
 const thirs = css`
